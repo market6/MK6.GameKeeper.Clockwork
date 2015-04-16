8291fe5e4c80bc74f637dcc688336132793b3cb3
Doug Mulley <doug.mulley@market6.com>
2015-04-16 11:22:53 -0500

    Initial commit

---
+++ MK6.GameKeeper.Clockwork/Program.cs
@@ -0,0 +1,41 @@
+﻿using Serilog;
+using System.Configuration;
+using Topshelf;
+
+namespace MK6.GameKeeper.Clockwork
+{
+    class Program
+    {
+        const string DefaultServiceName = "MK6.Clockwork";
+
+        const string DefaultDisplayName = "MK6.Clockwork";
+
+        const string DefaultDescription = "Scheduled task manager";
+
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.AppSettings()
+                .CreateLogger();
+
+            HostFactory.Run(config =>
+            {
+                config.Service<Scheduler>(
+                    host => new Scheduler(ConfigurationManager.AppSettings),
+                    svc =>
+                    {
+                        svc.BeforeStartingService(() => Log.Information("Starting service"));
+                        svc.AfterStartingService(() => Log.Information("Service started"));
+                        svc.BeforeStoppingService(() => Log.Information("Stopping service"));
+                        svc.AfterStoppingService(() => Log.Information("Service stopped"));
+                    });
+
+                config.RunAsLocalService();
+                config.SetServiceName(ConfigurationManager.AppSettings["Name"] ?? DefaultServiceName);
+                config.SetDisplayName(ConfigurationManager.AppSettings["DisplayName"] ?? DefaultDisplayName);
+                config.SetDescription(ConfigurationManager.AppSettings["Description"] ?? DefaultDescription);
+            });
+
+        }
+    }
+}
---
+++ MK6.GameKeeper.Clockwork/Scheduler.cs
@@ -0,0 +1,54 @@
+﻿using Quartz;
+using Quartz.Impl;
+using Serilog;
+using System;
+using System.Collections.Specialized;
+using Topshelf;
+
+namespace MK6.GameKeeper.Clockwork
+{
+    public class Scheduler : ServiceControl
+    {
+        private readonly IScheduler scheduler;
+
+        public Scheduler(NameValueCollection properties)
+        {
+            var schedulerFactory = new StdSchedulerFactory(properties);
+            this.scheduler = schedulerFactory.GetScheduler();
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            try
+            {
+                Log.Information("Starting scheduler");
+                this.scheduler.Start();
+                Log.Information("Scheduler started");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error occurred while starting scheduler");
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            try
+            {
+                Log.Information("Shutting down scheduler");
+                this.scheduler.Shutdown();
+                Log.Information("Schduler shut down");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error occurred while shutting down scheduler");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
