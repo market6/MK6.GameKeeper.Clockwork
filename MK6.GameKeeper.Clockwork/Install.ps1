3acce57d426e2757a73e6b1ce29a58a5b2df214b
Doug Mulley <doug.mulley@market6.com>
2015-04-17 08:49:12 -0500

    Added Install.ps1 to mark content as copy to output

---
+++ MK6.GameKeeper.Clockwork/Install.ps1
@@ -0,0 +1,10 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$contentFiles = "MK6.GameKeeper.Clockwork.exe.config", "quartz_jobs.xml"
+
+Foreach ($contentFile in $contentFiles)
+{
+	$file = $project.ProjectItems.Item($contentFile)
+	$copyToOutput = $file.Properties.Item("CopyToOutputDirectory")
+	$copyToOutput.Value = 2
+}
